e5798fb563b63c52b7c4ed3f04b8da75cbbe21b4
Jeziel Lopes <jeziellopes@gmail.com>
2026-04-03 20:46:35 -0300

    style(trading-layout): remove unnecessary padding from layout
    Signed-off-by: Jeziel Lopes <jeziellopes@gmail.com>

--- src/routes/symbol/-trading-layout.tsx
+++ src/routes/symbol/-trading-layout.tsx
@@ -82,7 +82,7 @@ export function TradingLayout({ symbol }: TradingLayoutProps) {
 
   return (
     <ErrorBoundary>
-      <div className="w-full px-3 pb-3 flex flex-col gap-0">
+      <div className="w-full pb-3 flex flex-col gap-0">
         <Suspense
           fallback={
             <div className="w-full h-[600px] grid grid-cols-12 gap-2 p-3">
